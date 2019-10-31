3399f298c255c4aa4993f3c3db41119478728284
Xavier Antoviaque <xavier@antoviaque.org>
2019-10-31 09:18:49 -0400

    Trigger item list change with button, display current item

--- audiochecklist.py
+++ audiochecklist.py
@@ -5,6 +5,7 @@ import time
 
 from kivy.app import App
 from kivy.core.audio import SoundLoader
+from kivy.uix.button import Button
 from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
@@ -30,6 +31,7 @@ class AudioChecklistApp(App):
         super(AudioChecklistApp, self).__init__(*args, **kwargs)
         self.speech_api = texttospeech.TextToSpeechClient()
         self.sound = None
+        self.checklist_items = self.get_checklist_items()
 
     def intro_wizard(self):
         """
@@ -41,44 +43,47 @@ class AudioChecklistApp(App):
 
     def build(self):
         b = BoxLayout(orientation='horizontal', spacing=10, width=1600, height=900)
-        l = Label(text=self.get_checklist_text(), font_size=20)
-        b.add_widget(l)
+        self.title_label = Label(text="Welcome", font_size=30)
+        b.add_widget(self.title_label)
+        checkbtn = Button(text='Next')
+        checkbtn.bind(on_release=self.show_next_checklist_item)
+        b.add_widget(checkbtn)
         return b
 
-    def get_checklist_text(self):
+    def show_next_checklist_item(self, obj):
+        checklist_item = self.checklist_items.pop(0)
+        self.title_label.text = checklist_item
+        self.text2audio(checklist_item)
+
+    def get_checklist_items(self):
         api = todoist.TodoistAPI(config['TODOIST_API_KEY'])
         api.sync()
         project = api.projects.get_by_id(config['TODOIST_PROJECT_ID'])
-        checklist_text = 'Checklist:\n\n'
-        for item in api.projects.get_data(config['TODOIST_PROJECT_ID'])['items']:
-            checklist_text += '{0}\n'.format(item['content'])
-            self.text2audio(item['content'])
-
-        return checklist_text
+        checklist = api.projects.get_data(config['TODOIST_PROJECT_ID'])
+        return [item['content'] for item in checklist['items']]
 
     def text2audio(self, text):
-            print(text)
+        print(text)
 
-            if self.sound:
-                self.sound.stop()
-                self.sound.unload()
+        if self.sound:
+            self.sound.stop()
+            self.sound.unload()
 
-            input_text = texttospeech.types.SynthesisInput(text=text)
-            voice = texttospeech.types.VoiceSelectionParams(
-                        language_code='en-US',
-                        name='en-US-Wavenet-F')
-            audio_config = texttospeech.types.AudioConfig(
-                        speaking_rate=1.3,
-                        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        input_text = texttospeech.types.SynthesisInput(text=text)
+        voice = texttospeech.types.VoiceSelectionParams(
+                    language_code='en-US',
+                    name='en-US-Wavenet-F')
+        audio_config = texttospeech.types.AudioConfig(
+                    speaking_rate=1.3,
+                    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-            response = self.speech_api.synthesize_speech(input_text, voice, audio_config)
+        response = self.speech_api.synthesize_speech(input_text, voice, audio_config)
 
-            with open('/tmp/output.mp3', 'wb') as out:
-                out.write(response.audio_content)
+        with open('/tmp/output.mp3', 'wb') as out:
+            out.write(response.audio_content)
 
-            self.sound = SoundLoader.load('/tmp/output.mp3')
-            self.sound.play()
-            time.sleep(3)
+        self.sound = SoundLoader.load('/tmp/output.mp3')
+        self.sound.play()
 
 
 # Main ##################################################################################

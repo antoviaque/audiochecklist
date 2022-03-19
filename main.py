29a38ff27e54ca5aa10ba85e4e2c66e3a5d9a3ec
Xavier Antoviaque <xavier@antoviaque.org>
2022-03-19 12:00:49 +0100

    Fix bug with checklist entries of same name & cleanup

--- main.py
+++ main.py
@@ -33,12 +33,32 @@ class AudioChecklistApp(App):
     def __init__(self, *args, **kwargs):
         super(AudioChecklistApp, self).__init__(*args, **kwargs)
         self.sound = None
-        self.selected_checklist_name = None
         self.buttons = []
+        self.selected_checklist = None
         self.update_checklist_items()
 
+    def update_checklist_items(self):
+        """
+        Update the current list of items in the checklist, based on `self.selected_checklist_name`
+        """
+        if self.selected_checklist is None:
+            self.selected_checklist = get_all_checklists().children[0]
+
+        self.selected_checklist_items_todo = list(self.selected_checklist[1:])
+        self.selected_checklist_items_done = []
+        self.current_item = None
+        print('Refreshed checklist items from selected checklist', self.selected_checklist.heading)
+
+    def get_checklists(self):
+        return get_all_checklists().children
+
     def get_checklists_names(self):
-        return [checklist.heading for checklist in get_all_checklists().children]
+        return [checklist.heading for checklist in self.get_checklists()]
+
+    def get_checklist_by_name(self, name):
+        for checklist in self.get_checklists():
+            if checklist.heading == name:
+                return checklist
 
     def build(self):
         self.body = BoxLayout(orientation='vertical', spacing=10, width=1600, height=900)
@@ -55,7 +75,7 @@ class AudioChecklistApp(App):
         """
         self.checklist_selector = Spinner(
             # default value shown
-            text=self.selected_checklist_name,
+            text=self.selected_checklist.heading,
             # available values
             values=self.get_checklists_names(),
             # positioning
@@ -65,7 +85,7 @@ class AudioChecklistApp(App):
 
         def on_selected_checklist_change(spinner, text):
             print('Checklist changed by user to', text)
-            self.selected_checklist_name = text
+            self.selected_checklist = self.get_checklist_by_name(text)
             self.update_checklist_items()
             self.reset_checklist_items_buttons()
             self.show_next_checklist_item(None)
@@ -88,34 +108,17 @@ class AudioChecklistApp(App):
         self.checkbtn.bind(on_release=self.show_next_checklist_item)
         self.body.add_widget(self.checkbtn)
 
-    def update_checklist_items(self):
-        """
-        Update the current list of items in the checklist, based on `self.selected_checklist_name`
-        """
-        all_checklists = get_all_checklists()
-        if self.selected_checklist_name is None:
-            self.selected_checklist_name = all_checklists.children[0].heading
-        
-        # Find the checklist with the same name
-        print('Updating checklist items from selected checklist', self.selected_checklist_name)
-        for checklist in all_checklists.children:
-            if self.selected_checklist_name == checklist.heading:
-                self.selected_checklist = checklist
-                self.selected_checklist_items = [item.heading for item in checklist][1:]
-                self.selected_checklist_items_done = []
-                self.current_item_name = None
-                print('Refreshed checklist items from selected checklist', self.selected_checklist_name)
-                print('Checklist items:', self.selected_checklist_items)
-
     def reset_checklist_items_buttons(self):
         # Clear existing buttons
         for button in self.buttons:
             self.checklist_items_buttons_box.remove_widget(button)
         self.buttons = []
 
-        for checklist_item in self.selected_checklist_items:
-            button = Button(text=checklist_item, size_hint=(1,0.02), halign='left')#, text_size=(1,1))
+        for checklist_item in self.selected_checklist_items_todo:
+            button = Button(text=checklist_item.heading, size_hint=(1,0.02), halign='left')
             button.text_size = [self.body.width, None]
+            button.checklist_item = checklist_item
+            checklist_item.button = button
             def callback(instance):
                 print('The button <%s> is being pressed' % instance.text)
             button.bind(on_press=callback)
@@ -123,29 +126,20 @@ class AudioChecklistApp(App):
             self.checklist_items_buttons_box.add_widget(button)
 
     def show_next_checklist_item(self, obj):
-        if not self.selected_checklist_items:
+        if not self.selected_checklist_items_todo:
             return # TODO: What do we do when we reach the end of the list?
 
-        done_item_name = self.current_item_name
-        self.selected_checklist_items_done.append(done_item_name)
-        self.current_item_name = self.selected_checklist_items.pop(0)
+        done_item = self.current_item
+        self.selected_checklist_items_done.append(done_item)
+        self.current_item = self.selected_checklist_items_todo.pop(0)
 
-        if done_item_name is not None:
-            done_item_button = self.find_button_by_name(done_item_name)
-            done_item_button.disabled = True
-            done_item_button.state = 'normal'
+        if done_item is not None:
+            done_item.button.disabled = True
+            done_item.button.state = 'normal'
 
-        current_item_button = self.find_button_by_name(self.current_item_name)
-        current_item_button.state = 'down'
+        self.current_item.button.state = 'down'
         
-        self.read_text(self.current_item_name)
-
-    def find_button_by_name(self, name):
-        for button in self.buttons:
-            if button.text == name:
-                return button
-        else:
-            raise IndexError(name)
+        self.read_text(self.current_item.heading)
 
     def read_text(self, text):
         if self.sound:
